245bc51c39a3ecaf7b133f3e0d43241925cc7616
許孝萱 <hsuhsiaohsuan@s-MacBook-Pro-2024.local>
2025-02-25 19:31:22 +0900

    add logo

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-1. use Google logo
-   1. https://about.google/brand-resource-center/brand-elements/
-2. use Instagram logo
-   1. https://about.meta.com/brand/resources/instagram/instagram-brand/
+Logo Policy:
+1. Google
+   https://about.google/brand-resource-center/brand-elements/
+2. Instagram
+   https://about.meta.com/brand/resources/instagram/instagram-brand/
+3. Facebook
+   https://about.meta.com/brand/resources/facebook/logo/
+4. X
+   https://about.x.com/en/who-we-are/brand-toolkit
+5. LinkedIn
+   https://brand.linkedin.com/en-us
+6. Microsoft
+7. Dropbox
+   https://www.dropbox.com/branding#:~:text=For%20the%20Dropbox%20logo%2C%20as,modify%20them%20in%20any%20way.
+8. Slack
+   https://slack.com/media-kit
+9. Github
+   https://github.com/logos
+10. Gitlab
+    https://about.gitlab.com/press/press-kit/
+11. Steam
+    https://steamcdn-a.akamaihd.net/steamcommunity/public/images/steamworks_docs/english/Steam_Guidelines_060222.pdf
--- app/src/main/java/idv/hsu/authenticator/data/entities/TotpDataItem.kt
+++ app/src/main/java/idv/hsu/authenticator/data/entities/TotpDataItem.kt
@@ -9,18 +9,26 @@ data class TotpDataItem(
     val issuer: String?,
     val remainingTime: Long
 ) {
-    fun getIssuerIcon(): Int {
-        return issuerIcons[issuer?.lowercase()] ?: R.drawable.ic_launcher_foreground
+    fun getIssuerIcon(isDarkTheme: Boolean = false): Int {
+        return when (issuer?.lowercase()) {
+            "adobe" -> R.drawable.logo_adobe
+            "apple" -> if (isDarkTheme) R.drawable.logo_apple_dark else R.drawable.logo_apple_light
+            "dropbox" -> R.drawable.logo_dropbox
+            "facebook" -> R.drawable.logo_facebook
+            "figma" -> R.drawable.logo_figma
+            "github" -> if (isDarkTheme) R.drawable.logo_github_dark else R.drawable.logo_github_light
+            "google" -> R.drawable.logo_google
+            "instagram" -> if (isDarkTheme) R.drawable.logo_instagram_dark else R.drawable.logo_instagram_light
+            "linkedin" -> R.drawable.logo_linkedin
+            "microsoft" -> R.drawable.logo_microsoft
+            "netflix" -> R.drawable.logo_netflix
+            "notion" -> R.drawable.logo_notion
+            "spotify" -> R.drawable.logo_spotify
+            "steam" -> R.drawable.logo_steam
+            "telegram" -> R.drawable.logo_telegram
+            "threads" -> if (isDarkTheme) R.drawable.logo_threads_dark else R.drawable.logo_threads_light
+            "x" -> if (isDarkTheme) R.drawable.logo_x_dark else R.drawable.logo_x_light
+            else -> R.drawable.ic_launcher_foreground
+        }
     }
 }
-
-private val issuerIcons = mapOf(
-    "google" to R.drawable.logo_google,
-    "github" to R.drawable.logo_google,
-    "microsoft" to R.drawable.logo_google,
-    "facebook" to R.drawable.logo_google,
-    "amazon" to R.drawable.logo_google,
-    "dropbox" to R.drawable.logo_google,
-    "twitter" to R.drawable.logo_google,
-    "instagram" to R.drawable.logo_instagram
-)
